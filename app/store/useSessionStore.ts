08af9f8e4bdd62c369d89c7bb658f164c1aa5301
daoddee <79615679+daoddee@users.noreply.github.com>
2025-10-04 17:46:41 +0100

    Update useSessionStore.ts

--- app/store/useSessionStore.ts
+++ app/store/useSessionStore.ts
@@ -1,1 +1,21 @@
+'use client';
+import { create } from 'zustand';
 
+
+type State = {
+examId?: string;
+startedAt?: number;
+duration?: number;
+};
+
+
+type Actions = {
+start: (examId: string, duration: number) => void;
+end: () => void;
+};
+
+
+export const useSessionStore = create<State & Actions>((set) => ({
+start: (examId, duration) => set({ examId, startedAt: Date.now(), duration }),
+end: () => set({ examId: undefined, startedAt: undefined, duration: undefined }),
+}));
