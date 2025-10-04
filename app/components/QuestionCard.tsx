00b2c15c736043a4e95179e23d0b0ab2982f93cb
daoddee <79615679+daoddee@users.noreply.github.com>
2025-10-04 17:23:26 +0100

    Update QuestionCard.tsx

--- app/components/QuestionCard.tsx
+++ app/components/QuestionCard.tsx
@@ -1,1 +1,11 @@
+import 'katex/dist/katex.min.css';
+import TeX from '@matejmazur/react-katex';
 
+
+export default function QuestionCard({ latex }: { latex: string }) {
+return (
+<div className="rounded-2xl p-6 shadow-sm bg-white dark:bg-zinc-900">
+<TeX math={latex} block />
+</div>
+);
+}
